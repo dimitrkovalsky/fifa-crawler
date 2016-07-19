68c8cab9417632c73dc4bd4f6ac32594a189ec1d
Dmytro Kovalskyi <dmytro_kovalskyi@epam.com>
2016-07-19 10:11:45 +0300

    Improved Data

--- src/main/java/com/liberty/common/UrlResolver.java
+++ src/main/java/com/liberty/common/UrlResolver.java
@@ -1,5 +1,7 @@
 package com.liberty.common;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static com.liberty.common.FifaEndpoints.AUCTION_HOUSE_URL;
 import static com.liberty.common.FifaEndpoints.BID_URL;
 import static com.liberty.common.FifaEndpoints.ITEM_URL;
@@ -10,60 +12,78 @@ import static com.liberty.common.FifaEndpoints.TRADE_LINE_URL;
 /**
  * User: Dimitr Date: 19.07.2016 Time: 9:19
  */
+@Slf4j
 public class UrlResolver {
+
   public static boolean externalUrl = false;
 
+  public static void setExternalUrl(boolean externalUrl) {
+    if (UrlResolver.externalUrl != externalUrl) {
+      log.info("Changing source externalUrl = " + externalUrl);
+      UrlResolver.externalUrl = externalUrl;
+    }
+  }
+
   public static String getItemUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.ITEM_URL;
+    }
     return ITEM_URL;
   }
 
   public static String getAuctionHouseUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.AUCTION_HOUSE_URL;
+    }
     return AUCTION_HOUSE_URL;
   }
 
   public static String getRemoveSold() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.REMOVE_SOLD;
+    }
     return FifaEndpoints.REMOVE_SOLD;
   }
 
   public static String getRelistUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.RELIST;
+    }
     return FifaEndpoints.RELIST;
   }
 
   public static String getGetUnassignedUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.GET_UNASSIGNED;
+    }
     return FifaEndpoints.GET_UNASSIGNED;
   }
 
   public static String getStatusUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.STATUS_URL;
+    }
     return STATUS_URL;
   }
 
   public static String getBidUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.BID_URL;
+    }
     return BID_URL;
   }
 
   public static String getTradeLineUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.TRADE_LINE_URL;
+    }
     return TRADE_LINE_URL;
   }
 
   public static String getSearchUrl() {
-    if (externalUrl)
+    if (externalUrl) {
       return FifaExternalEndpoints.SEARCH_URL;
+    }
     return SEARCH_URL;
   }
 }
